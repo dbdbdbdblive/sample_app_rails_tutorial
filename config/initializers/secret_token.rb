c2e468b2cc413cc65d4d7244c25f05ed9f2fc0a8
db <dbdbdbdblive@gmail.com>
2014-03-04 14:44:44 -0800

    Initial commit.  App created with rails new sample_app --skip-test-unit , and .gitignore updated to match railstutorial listing 1.7 (to be more secure by not exposing the .secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '1e3dc972be9602af24ecc4a77b2411f122e5cea49c9ca088a10395beb6b03ab5e07fdd363d8fdf66e3a5ffc9d27b99b54d9fcc69c9ac2cc6b6ac128db461c3b1'
