d339feb0ef8e22becfd919e74d1e2e12071d1b20
db <dbdbdbdblive@gmail.com>
2014-03-05 15:59:14 -0800

    3.5, Excercise 2: DRY out the static_pages_spec by using let to store the common base string in all pages' titles.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:title_base) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "Static pages" do
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{title_base} | Home")
     end
     
   end
@@ -23,7 +25,7 @@ describe "Static pages" do
     
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{title_base} | Help")
     end
   end
   
@@ -35,7 +37,7 @@ describe "Static pages" do
     
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{title_base} | About")
     end
   end
   
@@ -47,7 +49,7 @@ describe "Static pages" do
     
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{title_base} | Contact")
     end
   end
 end
