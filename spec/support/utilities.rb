8a30567e8f428212e03382edc5ef0d11a8d7cc27
db <dbdbdbdblive@gmail.com>
2014-03-07 14:59:29 -0800

    5.6: Exercises * 1. use have_selector to test that "Contact" is matched specifically in an <h1>, not just anywhere on the page. * 2. add shared_example_for "all static pages" to clean up reduce repetition in each of the pages' checks * 3. add tests in stagic_pages_spec for following the links * 4. add tests for ApplicationHelper, and remove redundant full_title implementation in spec/support/utilities - use ApplicationHelper mixing instead

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,7 @@
+include ApplicationHelper
+# Implementation no long needs to be here; since it duplicates the full_title
+# method in ApplicationHelper, we just mix in ApplicationHelper
+=begin
 # Returns the full title on a per-page basis
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
@@ -7,3 +11,4 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+=end
