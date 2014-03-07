f11ffbf6d3d7f413e75814e62ac15a5f55967866
db <dbdbdbdblive@gmail.com>
2014-03-07 14:22:47 -0800

    5.4 User signup: First Step * Add users controller * Add route for signup * Add basic tests to user_pages for sign up * Add link to signup on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
-  #get "static_pages/home"
+
   root 'static_pages#home'
-  #get "static_pages/help"
   match '/help', to: 'static_pages#help', via: 'get'
-  #get "static_pages/about"
   match '/about', to: 'static_pages#about', via: 'get'
-  #get "static_pages/contact"
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  get "/users/new"
+  match '/signup', to: 'users#new', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
